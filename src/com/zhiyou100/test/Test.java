5dad0cec304e920a9f83735a0e24bc87bccba658
ygy <872596463@qq.com>
2018-01-05 16:45:54 +0800

    新建了项目

---
+++ src/com/zhiyou100/test/Test.java
@@ -0,0 +1,16 @@
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:32
+*  
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("新建测试项目");
+	}
+}
+
